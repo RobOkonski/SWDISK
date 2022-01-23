d06a0766ff9245743e5db478475eec72b389db41
Student 241540 <241540@student.pwr.edu.pl>
2022-01-23 19:06:45 +0100

    Add new datafiles

--- SWDISK/Program.cs
+++ SWDISK/Program.cs
@@ -9,11 +9,11 @@
     {
         static void Main()
         {
-            string[] dataFileName = { "NEH1.DAT", "NEH2.DAT", "NEH3.DAT", "NEH4.DAT", "NEH5.DAT", "NEH6.DAT", "NEH7.DAT" };
+            string[] dataFileName = { "NEH1.DAT", "NEH2.DAT", "NEH3.DAT", "NEH4.DAT", "NEH10.DAT", "NEH11.DAT", "NEH12.DAT", "NEH13.DAT", "NEH14.DAT", "NEH15.DAT", "NEH5.DAT", "NEH6.DAT", "NEH7.DAT", "NEH8.DAT", "NEH9.DAT" };
             //string[] resultFileName = { "NEH1.OUT", "NEH2.OUT", "NEH3.OUT", "NEH4.OUT", "NEH5.OUT", "NEH6.OUT", "NEH7.OUT" };
             string testDataDir = "C:\\Users\\Student241540\\source\\repos\\SWDISK\\SWDISK\\Neh";
 
-            for (int i=0; i<7; i++)
+            for (int i=0; i<4; i++)
             {
                 var dataFilePath = Path.Combine(testDataDir, dataFileName[i]);
                 //var resultFilePath = Path.Combine(testDataDir, resultFileName[i]);
@@ -48,6 +48,33 @@
                 Console.WriteLine($"Brute force result: {bruteForceTime} in {bruteElapsedTime}");
 
                 //Console.WriteLine($"Time: {nehTime}, expected time: {expectedTotalTime}, original sequence time: {origSeqTime}, brute force time: {bruteForceTime}");
+            }
+
+            for (int i = 4; i < 15; i++)
+            {
+                var dataFilePath = Path.Combine(testDataDir, dataFileName[i]);
+                //var resultFilePath = Path.Combine(testDataDir, resultFileName[i]);
+
+                var tasks = LoadTasks(dataFilePath);
+                //var expectedTotalTime = DataLoader.LoadExpectedResult(resultFilePath);
+
+                Console.WriteLine($"Dataset{i}");
+
+                Stopwatch nehStopWatch = new Stopwatch();
+                nehStopWatch.Start();
+                var (nehTime, _) = Neh.Calculate(tasks);
+                nehStopWatch.Stop();
+                TimeSpan nehTs = nehStopWatch.Elapsed;
+                string nehElapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", nehTs.Hours, nehTs.Minutes, nehTs.Seconds, nehTs.Milliseconds);
+                Console.WriteLine($"Neh result: {nehTime} in {nehElapsedTime}");
+
+                Stopwatch origStopWatch = new Stopwatch();
+                origStopWatch.Start();
+                var origSeqTime = OriginalSequence.Calculate(tasks);
+                origStopWatch.Stop();
+                TimeSpan origTs = origStopWatch.Elapsed;
+                string origElapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", origTs.Hours, origTs.Minutes, origTs.Seconds, origTs.Milliseconds);
+                Console.WriteLine($"Original sequence result: {origSeqTime} in {origElapsedTime}");
             }
 
         }
