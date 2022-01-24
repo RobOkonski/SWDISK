21d80d30a91be29b5eda249e4b05b13a9c18262d
Krzysztof Sitarz <chris5424@protonmail.com>
2022-01-25 00:01:25 +0100

    Ukończono testy

--- SWDISK/Program.cs
+++ SWDISK/Program.cs
@@ -4,88 +4,104 @@
     using System.IO;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Threading.Tasks;
 
     class Program
     {
         static void Main()
         {
             string[] dataFileName = { "NEH1.DAT", "NEH2.DAT", "NEH3.DAT", "NEH4.DAT", "NEH10.DAT", "NEH11.DAT", "NEH12.DAT", "NEH13.DAT", "NEH14.DAT", "NEH15.DAT", "NEH5.DAT", "NEH6.DAT", "NEH7.DAT", "NEH8.DAT", "NEH9.DAT" };
-            string testDataDir = "C:\\Users\\Student241540\\source\\repos\\SWDISK\\SWDISK\\Neh";
+            string testDataDir = "../../../Neh";
 
-            for (int i=0; i<4; i++)
+            int testCount = 10;
+            int repeatCount = 10;
+            using (BinaryWriter w = new BinaryWriter(File.Open("../../../Wyniki.txt", FileMode.Create)))
             {
-                var dataFilePath = Path.Combine(testDataDir, dataFileName[i]);
+                for (int i = 0; i < 15; i++)
+                {
+                    var dataFilePath = Path.Combine(testDataDir, dataFileName[i]);
+                    var tasks = LoadTasks(dataFilePath);
 
-                var tasks = LoadTasks(dataFilePath);
+                    Console.WriteLine("\n***************");
+                    Console.WriteLine($"Dataset {i}");
+                    Console.WriteLine("***************\n");
 
-                Console.WriteLine($"Dataset{i}");
+                    w.Write("\n***************\n");
+                    w.Write($"Dataset {i}\n");
+                    w.Write("***************\n\n");                   
 
-                Stopwatch nehStopWatch = new Stopwatch();
-                nehStopWatch.Start();
-                var (nehTime, _) = Neh.Calculate(tasks);
-                nehStopWatch.Stop();
-                TimeSpan nehTs = nehStopWatch.Elapsed;
-                string nehElapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", nehTs.Hours, nehTs.Minutes, nehTs.Seconds, nehTs.Milliseconds);
-                Console.WriteLine($"Neh result: {nehTime} in {nehElapsedTime}");
 
-                Stopwatch origStopWatch = new Stopwatch();
-                origStopWatch.Start();
-                var origSeqTime = OriginalSequence.Calculate(tasks);
-                origStopWatch.Stop();
-                TimeSpan origTs = origStopWatch.Elapsed;
-                string origElapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", origTs.Hours, origTs.Minutes, origTs.Seconds, origTs.Milliseconds);
-                Console.WriteLine($"Original sequence result: {origSeqTime} in {origElapsedTime}");
+                    for (int j = 0; j < testCount; j++)
+                    {
+                        Stopwatch nehStopWatch = new Stopwatch();
+                        nehStopWatch.Start();
+                        int nehTimeSum = 0;
+                        for (int t = 0; t < repeatCount; t++)
+                        {
+                            var (nehTime, _) = Neh.Calculate(tasks);
+                            nehTimeSum = nehTime;
+                        }
+                        nehStopWatch.Stop();
+                        long nehMili = nehStopWatch.ElapsedMilliseconds;
+                        Console.WriteLine($"Neh result run {j}: {nehTimeSum} in {nehMili} miliseconds");
+                        w.Write($"Neh result run {j}: {nehTimeSum} in {nehMili} miliseconds\n");
+                    }
 
-                Stopwatch geneticStopWatch = new Stopwatch();
-                geneticStopWatch.Start();
-                var (geneticSeqTime, _) = Genetic.Calculate(tasks);
-                geneticStopWatch.Stop();
-                TimeSpan geneticTs = geneticStopWatch.Elapsed;
-                string geneticElapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", geneticTs.Hours, geneticTs.Minutes, geneticTs.Seconds, geneticTs.Milliseconds);
-                Console.WriteLine($"Genetic result: {geneticSeqTime} in {geneticElapsedTime}");
+                    for (int j = 0; j < testCount; j++)
+                    {
+                        Stopwatch origStopWatch = new Stopwatch();
+                        origStopWatch.Start();
+                        int origSeqTimeSum = 0;
+                        for (int t = 0; t < repeatCount; t++)
+                        {
+                            var origSeqTime = OriginalSequence.Calculate(tasks);
+                            origSeqTimeSum = origSeqTime;
+                        }
+                        origStopWatch.Stop();
+                        long origMili = origStopWatch.ElapsedMilliseconds;
 
-                Stopwatch bruteStopWatch = new Stopwatch();
-                bruteStopWatch.Start();
-                var (bruteForceTime, _) = BruteForce.Calculate(tasks);
-                bruteStopWatch.Stop();
-                TimeSpan bruteTs = bruteStopWatch.Elapsed;
-                string bruteElapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", bruteTs.Hours, bruteTs.Minutes, bruteTs.Seconds, bruteTs.Milliseconds);
-                Console.WriteLine($"Brute force result: {bruteForceTime} in {bruteElapsedTime}");
+                        Console.WriteLine($"Original sequence result run {j}: {origSeqTimeSum} in {origMili} miliseconds");
+                        w.Write($"Original sequence result run {j}: {origSeqTimeSum} in {origMili} miliseconds\n");
+
+                    }
+
+                    for (int j = 0; j < testCount; j++)
+                    {
+                        Stopwatch geneticStopWatch = new Stopwatch();
+                        geneticStopWatch.Start();
+                        int geneticSeqTimeSum = 0;
+                        for (int t = 0; t < repeatCount; t++)
+                        {
+                            var (geneticSeqTime, _) = Genetic.Calculate(tasks);
+                            geneticSeqTimeSum = geneticSeqTime;
+                        }
+                        geneticStopWatch.Stop();
+                        long geneticMili = geneticStopWatch.ElapsedMilliseconds;
+                        
+                        Console.WriteLine($"Genetic result run {j}: {geneticSeqTimeSum} in {geneticMili} miliseconds");
+                        w.Write($"Genetic result run {j}: {geneticSeqTimeSum} in {geneticMili} miliseconds\n");
+                    }
+
+                    if (i < 4)
+                    {
+                        for (int j = 0; j < testCount; j++)
+                        {
+                            Stopwatch bruteStopWatch = new Stopwatch();
+                            bruteStopWatch.Start();
+                            int bruteForceTimeSum = 0;
+                            for (int t = 0; t < repeatCount; t++)
+                            {
+                                var (bruteForceTime, _) = BruteForce.Calculate(tasks);
+                                bruteForceTimeSum = bruteForceTime;
+                            }
+                            bruteStopWatch.Stop();
+                            long bruteMili = bruteStopWatch.ElapsedMilliseconds;
+                            Console.WriteLine($"Brute force result run {j}: {bruteForceTimeSum} in {bruteMili} miliseconds");
+                            w.Write($"Brute force result run {j}: {bruteForceTimeSum} in {bruteMili} miliseconds\n");
+                        }
+                    }
+                }
             }
-
-            for (int i = 4; i < 15; i++)
-            {
-                var dataFilePath = Path.Combine(testDataDir, dataFileName[i]);
-
-                var tasks = LoadTasks(dataFilePath);
-
-                Console.WriteLine($"Dataset{i}");
-
-                Stopwatch nehStopWatch = new Stopwatch();
-                nehStopWatch.Start();
-                var (nehTime, _) = Neh.Calculate(tasks);
-                nehStopWatch.Stop();
-                TimeSpan nehTs = nehStopWatch.Elapsed;
-                string nehElapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", nehTs.Hours, nehTs.Minutes, nehTs.Seconds, nehTs.Milliseconds);
-                Console.WriteLine($"Neh result: {nehTime} in {nehElapsedTime}");
-
-                Stopwatch origStopWatch = new Stopwatch();
-                origStopWatch.Start();
-                var origSeqTime = OriginalSequence.Calculate(tasks);
-                origStopWatch.Stop();
-                TimeSpan origTs = origStopWatch.Elapsed;
-                string origElapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", origTs.Hours, origTs.Minutes, origTs.Seconds, origTs.Milliseconds);
-                Console.WriteLine($"Original sequence result: {origSeqTime} in {origElapsedTime}");
-
-                Stopwatch geneticStopWatch = new Stopwatch();
-                geneticStopWatch.Start();
-                var (geneticSeqTime, _) = Genetic.Calculate(tasks);
-                geneticStopWatch.Stop();
-                TimeSpan geneticTs = geneticStopWatch.Elapsed;
-                string geneticElapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", geneticTs.Hours, geneticTs.Minutes, geneticTs.Seconds, geneticTs.Milliseconds);
-                Console.WriteLine($"Genetic result: {geneticSeqTime} in {geneticElapsedTime}");
-            }
-
         }
 
         static List<FlowTask> LoadTasks(string filePath)
